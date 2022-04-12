7b168e85204522d228d8668cd110486eb9a51750
Korneliusz Łapacz <lapacz.kornel@gmail.com>
2022-04-12 21:14:02 +0200

    WIP read file

--- timer-v.v
+++ timer-v.v
@@ -19,18 +19,17 @@ struct Config {
 }
 
 fn get_config_path() string {
-	if os.args.len > 0 {
-		return os.args[0]
+	if os.args.len > 1 {
+		return os.args[1]
 	}
 
 	// TODO: get from "$HOME/.config/timer.json"
 	return './example.json'
 }
 
-fn read_config(path string) Config {
+fn read_config(path string) ?Config {
 	raw := os.read_file(path) or {
-		eprintln('Failed to read config file at path $path: $err')
-		return Config{}
+		return error('Failed to read config file at path "$path": $err')
 	}
 
 	return Config{}
@@ -38,7 +37,7 @@ fn read_config(path string) Config {
 
 fn main() {
 	config_path := get_config_path()
-	config := read_config(config_path)
+	config := read_config(config_path) ?
 
-	println('Hello World!')
+	println(config)
 }
